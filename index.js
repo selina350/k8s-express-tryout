44b9bcee09301331e7e5548904d394244f2dc412
selina350 <selina350.pub@gmail.com>
2024-04-03 22:46:58 -0400

    config message using .env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const message = process.env.RESPONSE_MESSAGE
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(message || "Hello World!")
 })
 
 app.listen(port, () => {
